39657e9cbbceb89a6d6d1ba02c8575faef6f3079
edilson junior <edilsonjrdev99@gmail.com>
2026-09-02 23:14:57 -0300

    feat: Ajusta nome do arquivo de teste e adiciona testes de get, put e delete

--- tests/Unit/HttpClientTest.php
+++ tests/Unit/HttpClientTest.php
@@ -5,9 +5,10 @@ namespace Tests\Unit;
 use App\Clients\HttpClient;
 use App\Configurations\MelhorEnvioConfig;
 use App\Contracts\HttpClientInterface;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
-class HttpClientTeste extends TestCase {
+class HttpClientTest extends TestCase {
   private HttpClientInterface $adapterMock;
   private MelhorEnvioConfig $melhorEnvioConfigMock;
   private HttpClient $httpClient;
@@ -18,32 +19,39 @@ class HttpClientTeste extends TestCase {
     $this->httpClient            = new HttpClient($this->adapterMock, $this->melhorEnvioConfigMock);
   }
 
-  public function test_deve_retornar_todos_os_headers_quando_post_for_chamado(): void {
-    // Arrange
+  #[DataProvider('httpMethodsProvider')]
+  public function test_deve_adicionar_headers_em_todos_os_metodos(string $method, array $options): void {
+    // arrange
     $email = 'teste@email.com';
 
     $this->melhorEnvioConfigMock
       ->method('getEmail')
       ->willReturn($email);
 
-    $expectedOptions = [
-      'json'    => ['key' => 'value'],
+    $expectedOptions = array_merge($options, [
       'headers' => [
         'Accept'       => 'application/json',
         'Content-Type' => 'application/json',
         'User-Agent'   => "Aplicação ($email)",
       ],
-    ];
+    ]);
 
     // assert
     $this->adapterMock
       ->expects($this->once())
-      ->method('post')
+      ->method($method)
       ->with('https://url.com', $expectedOptions);
 
     // act
-    $this->httpClient->post('https://url.com', [
-      'json' => ['key' => 'value']
-    ]);
+    $this->httpClient->$method('https://url.com', $options);
+  }
+
+  public static function httpMethodsProvider(): array {
+    return [
+      'get'    => ['get',    []],
+      'post'   => ['post',   ['json' => ['key' => 'value']]],
+      'put'    => ['put',    ['json' => ['key' => 'value']]],
+      'delete' => ['delete', []],
+    ];
   }
 }
